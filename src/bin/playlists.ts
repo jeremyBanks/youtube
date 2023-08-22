dee9721dbab93fee4647e8c126e25632eebc0ed8
Jeremy Banks <_@jeremy.ca>
2023-08-21 23:46:11 -0400

    how's this

---
+++ src/bin/playlists.ts
@@ -0,0 +1,105 @@
+// deno-lint-ignore-file no-explicit-any
+import yaml from "../yaml.ts";
+import { youtubei } from "../youtube.ts";
+
+const catalogueData = yaml.load("catalogue.yaml");
+const campaignData = yaml.load("campaigns.yaml") as Array<{
+  season: string;
+  from: "Dimension 20";
+  debut: string;
+  cast?: string;
+  world?: string;
+  videos: Array<{
+    trailer?: string;
+    episode?: string;
+    animate?: string;
+    insight?: string;
+    special?: string;
+    public?: string;
+    members?: string;
+  }>;
+}>;
+const playlistSpecs = yaml.load("playlists.yaml") as Array<{
+  name: string;
+  id?: string;
+  description: string;
+  include: {
+    from: "Dimension 20";
+    season?: string;
+    type: Array<"episode" | "special" | "trailer" | "insight" | "animate">;
+    version: Array<"public" | "members">;
+  };
+}>;
+let playlistMd = "# Playlists\n\n";
+
+for (const playlist of playlistSpecs) {
+  if (!playlist.id) {
+    break;
+  }
+
+  playlistMd += `## [${playlist.name}](https://www.youtube.com/playlist?list=${
+    playlist.id
+  })
+
+> ${playlist.description.trim().replaceAll("\n", "\n> ")}
+
+`;
+
+  let videos: Array<{
+    id: string;
+    title: string;
+  }> = [];
+
+  for (const campaign of campaignData) {
+    for (const video of campaign.videos) {
+      const type = video.animate
+        ? "animate"
+        : video.episode
+        ? "episode"
+        : video.special
+        ? "special"
+        : video.insight
+        ? "insight"
+        : video.trailer
+        ? "trailer"
+        : ("error" as any);
+      const title =
+        video.episode ??
+        video.animate ??
+        video.special ??
+        video.insight ??
+        video.trailer ??
+        "error";
+      const url = video.public ?? video.members ?? "error";
+      const id = url.replace("https://youtu.be/", "");
+
+      if (
+        playlist.include.season &&
+        playlist.include.season != campaign.season
+      ) {
+        continue;
+      }
+
+      if (!playlist.include.type.includes(type)) {
+        continue;
+      }
+
+      if (!playlist.include.version.includes("members") && !video.public) {
+        continue;
+      }
+
+      videos.push({ id, title });
+    }
+  }
+
+  for (const video of videos) {
+    playlistMd += `- [${video.title}](https://www.youtube.com/watch?v=${video.id}&list=${playlist.id})\n`;
+  }
+
+  playlistMd += "\n";
+
+  console.log(playlist);
+}
+
+console.log(playlistMd);
+Deno.writeTextFileSync("playlists.md", playlistMd);
--- src/yaml.ts
+++ src/yaml.ts
@@ -4,14 +4,14 @@ import { JsonValue } from "https://deno.land/std@0.198.0/json/mod.ts";
 export const load = (path: string): Array<JsonValue> =>
   yaml.parseAll(Deno.readTextFileSync(path), {
     schema: yaml.JSON_SCHEMA,
-  });
+  }) as Array<JsonValue>;
 
 export const dump = (path: string, items: Array<JsonValue>) =>
   Deno.writeTextFileSync(
     path,
     items
       .map((x) =>
-        yaml.stringify(x, {
+        yaml.stringify(x as any, {
           noCompatMode: true,
           noArrayIndent: true,
           lineWidth: -1,

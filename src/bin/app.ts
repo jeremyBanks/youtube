eaa054124dd96866e20ab8c35afec08f0987b37a
Jeremy Banks <_@jeremy.ca>
2023-09-13 12:15:04 -0400

    This is not worth focusing on

--- src/bin/app.ts
+++ src/bin/app.ts
@@ -1,26 +1,65 @@
 // deno-lint-ignore-file no-unused-vars
 import { App } from "../app.ts";
-const app = new App();
 
-const src = Deno.args
-  .map((src) => {
-    if (/^[A-Za-z_$][A-Za-z_$0-9]*$/.test(src)) {
-      return `app.${src}();`;
-    } else if (/^\.[A-Za-z_$]/.test(src)) {
-      return `app${src}`;
+const { positional, named } = (() => {
+  const positional: Array<string> = [];
+  const named: Record<string, string> = {};
+
+  for (let argument of Deno.args) {
+    if (argument.startsWith("--")) {
+      argument = argument.slice(2);
+      const equalsIndex = argument.indexOf("=");
+
+      let name = equalsIndex > -1 ? argument.slice(0, equalsIndex) : argument;
+      name = name.replace(
+        /([a-z])\-([a-z])/g,
+        (_, a, b) => a + b.toUpperCase()
+      );
+
+      const value = equalsIndex > -1 ? argument.slice(equalsIndex + 1) : name;
+      named[name] = value;
     } else {
-      return src;
+      positional.push(argument);
     }
-  })
-  .join(" ");
+  }
 
-console.log(`%c> %c${src}`, "color: grey", "color: default");
+  return {
+    positional,
+    named,
+  };
+})();
+
+const actualPlaylists = new App();
+
+if (positional.length === 0) {
+  console.error("ERROR: no subcommand specified");
+  Deno.exit(1);
+}
+
+const method = positional.shift()!;
+
+const args = [named, ...positional];
+
+if (args.length === 1 && Object.keys(named).length === 0) {
+  args.pop();
+}
+
+const src = `actualPlaylists.${method}(${args
+  .map((x) => JSON.stringify(x, null, 2))
+  .join(", ")})`;
+console.log(
+  `%c> %c${src.replaceAll("\n", "\n  ")}`,
+  "color: grey",
+  "color: default"
+);
 
 try {
-  const result = await eval(src);
+  const result = await (actualPlaylists as any)[method](...args);
 
   if (result !== undefined) {
     console.log("%c=", "color: green", result);
+  } else {
+    console.log("%c= done", "color: green");
   }
   Deno.exit(0);
 } catch (error) {
